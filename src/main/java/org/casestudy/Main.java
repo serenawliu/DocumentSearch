3ddc6390ecac60e26d4aac7a9f97a033a3a87b7c
Serena Liu (sl362) <sl362@duke.edu>
2022-08-06 23:13:02 -0600

    Implemented first pass of regex search & updated file read error checking.

--- src/main/java/org/casestudy/Main.java
+++ src/main/java/org/casestudy/Main.java
@@ -1,11 +1,15 @@
 package org.casestudy;
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         SearchEngine engine = new SearchEngine();
-        File f = new File("sample_text/french_armed_forces.txt");
-        System.out.println("Matching: " + engine.Search("defeat", f, SearchEngine.SearchMethod.STRING_SEARCH));
+
+        engine.addTextFilePath("data/french_armed_forces.txt");
+        engine.addTextFilePath("data/hitchhikers.txt");
+        engine.addTextFilePath("data/warp_drive.txt");
+
+        engine.printMap(engine.search("and", SearchEngine.searchMethod.STRING_SEARCH));
+        engine.printMap(engine.search("AND", SearchEngine.searchMethod.REGEX_SEARCH));
     }
 }
--- src/main/java/org/casestudy/SearchEngine.java
+++ src/main/java/org/casestudy/SearchEngine.java
@@ -1,46 +1,116 @@
 package org.casestudy;
 
 import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SearchEngine {
+    private static Set<String> textFilePaths = new HashSet<>();
 
-    public enum SearchMethod{
-        STRING_SEARCH,
-        REGEX_SEARCH,
-        PREPROCESS_SEARCH
+    public enum searchMethod {
+        STRING_SEARCH, REGEX_SEARCH, PREPROCESS_SEARCH
     }
 
-    public int Search(String stringToMatch, File f, SearchMethod s) throws IOException {
-        switch(s){
+    public void addTextFilePath(String filePath) {
+
+        if (new File(filePath).isFile()) {
+            textFilePaths.add(filePath);
+        } else {
+            System.out.println("WARNING: File " + filePath + " could not be found. Check the file path.");
+        }
+    }
+
+    public void printMap(LinkedHashMap<String, Integer> map) {
+
+        for (String fileName : map.keySet()) {
+            System.out.println(fileName + " " + map.get(fileName));
+        }
+    }
+
+    public LinkedHashMap<String, Integer> sortByRelevance(Map<String, Integer> unsortedMap) {
+
+        LinkedHashMap<String, Integer> relevanceSortedMap = new LinkedHashMap<>();
+        unsortedMap.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> relevanceSortedMap.put(x.getKey(), x.getValue()));
+
+        return relevanceSortedMap;
+    }
+
+    public LinkedHashMap<String, Integer> search(String stringToMatch, searchMethod s) throws IOException {
+        LinkedHashMap<String, Integer> hm = new LinkedHashMap<>();
+
+        switch (s) {
             case STRING_SEARCH:
-                return StringSearch(stringToMatch, f);
+                for (String filePath : textFilePaths) {
+                    String[] paths = filePath.split("/");
+                    hm.put(paths[paths.length - 1], stringSearch(stringToMatch.toLowerCase(), filePath));
+                }
+                break;
             case REGEX_SEARCH:
+                for (String filePath : textFilePaths) {
+                    String[] paths = filePath.split("/");
+                    hm.put(paths[paths.length - 1], regexSearch(stringToMatch.toLowerCase(), filePath));
+                }
+                break;
             case PREPROCESS_SEARCH:
+                for (String filePath : textFilePaths) {
+                    String[] paths = filePath.split("/");
+                    hm.put(paths[paths.length - 1], preprocessSearch(stringToMatch.toLowerCase(), filePath));
+                }
+                break;
             default:
                 break;
         }
-        return 0;
+        return sortByRelevance(hm);
     }
-/*
-Method to parse through file and search for a string
-@param: stringToMatch - target string to find in file
-@param: fileToSearch - file to search for matching string
-@return: int indicating occurrences of exact string match
- */
-    public int StringSearch(String stringToMatch, File fileToSearch) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileToSearch));
-        String currentLine;
 
+    /*
+    Method to parse through file and search for a target string.
+    @param: stringToMatch - target string to find in file
+    @param: fileToSearch - file to search for matching string
+    @return: int indicating occurrences of exact string match
+     */
+    public int stringSearch(String stringToMatch, String filePath) throws IOException {
+        File textFile = new File(filePath);
+        BufferedReader br = new BufferedReader(new FileReader(textFile));
+        String currentLine;
         int matching_string_count = 0;
-        while ((currentLine = br.readLine()) != null)
-        {
-            if (currentLine.contains(stringToMatch)){
-                matching_string_count++;
+
+        while ((currentLine = br.readLine()) != null) {
+            if (currentLine.contains(stringToMatch)) {
+                ++matching_string_count;
             }
         }
+        return matching_string_count;
+    }
+    /*
+    Method to parse through file and search for a target string using regex.
+    @param: stringToMatch - target string to find in file
+    @param: fileToSearch - file to search for matching string
+    @return: int indicating occurrences of exact string match
+     */
+    public int regexSearch(String stringToMatch, String filePath) throws IOException {
+        File textFile = new File(filePath);
+        BufferedReader br = new BufferedReader(new FileReader(textFile));
+        String currentLine;
 
+        Pattern pattern = Pattern.compile(stringToMatch, Pattern.LITERAL);
+        Matcher matcher;
+        int matching_string_count = 0;
+
+        while ((currentLine = br.readLine()) != null) {
+            matcher = pattern.matcher(currentLine);
+            if (matcher.find()) {
+                ++matching_string_count;
+            }
+        }
         return matching_string_count;
     }
 
-
+    public int preprocessSearch(String stringToMatch, String filePath) {
+        int matching_string_count = 0;
+        String[] directories = filePath.split("/");
+        String fileName = directories[directories.length-1];
+        return matching_string_count;
+    }
 }
